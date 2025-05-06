3340a8a53e4d4ab4e0c0e0d0e715d56246081799
kasandra-CA <115677642+kasandra-CA@users.noreply.github.com>
2025-05-06 01:33:39 -0600

    cambio en model por google

--- models/authModel.js
+++ models/authModel.js
@@ -3,57 +3,57 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: {
-    type: String,
-    required: function () {
-      return !this.isGoogleAccount;
-    }
-  },
-  phone: {
-    type: String,
-    required: function () {
-      return !this.isGoogleAccount;
-    }
-  },
-  pin: {
-    type: String,
-    minlength: 6,
-    maxlength: 6,
-    required: function () {
-      return !this.isGoogleAccount;
-    }
-  },
-  firstName: { type: String, required: true },
-  lastName: {
-    type: String,
-    required: function () {
-      return !this.isGoogleAccount;
-    }
-  },
-  country: { type: String },
-  birthdate: {
-    type: Date,
-    required: function () {
-      return !this.isGoogleAccount;
-    }
-  },
-  status: {
-    type: String,
-    enum: ["pending", "active"],
-    default: "pending"
-  },
-  verificationToken: { type: String },
-  googleId: { type: String },
-  isGoogleAccount: { type: Boolean, default: false }
+    email: { type: String, required: true, unique: true },
+    password: {
+        type: String,
+        required: function () {
+            return !this.isGoogleAccount;
+        }
+    },
+    phone: {
+        type: String,
+        required: function () {
+            return !this.isGoogleAccount;
+        }
+    },
+    pin: {
+        type: String,
+        minlength: 6,
+        maxlength: 6,
+        required: function () {
+            return !this.isGoogleAccount;
+        }
+    },
+    firstName: { type: String, required: true },
+    lastName: {
+        type: String,
+        required: function () {
+            return !this.isGoogleAccount;
+        }
+    },
+    country: { type: String },
+    birthdate: {
+        type: Date,
+        required: function () {
+            return !this.isGoogleAccount;
+        }
+    },
+    status: {
+        type: String,
+        enum: ["pending", "active"],
+        default: "pending"
+    },
+    verificationToken: { type: String },
+    googleId: { type: String },
+    isGoogleAccount: { type: Boolean, default: false }
 });
 
 // 🔐 Hashear la contraseña antes de guardar si no es cuenta Google
 userSchema.pre('save', async function (next) {
-  if (!this.isGoogleAccount && this.isModified('password')) {
-    this.password = await bcrypt.hash(this.password, 10);
-  }
-  next();
+    if (!this.isGoogleAccount && this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+    next();
 });
 
 module.exports = mongoose.model('User', userSchema);
